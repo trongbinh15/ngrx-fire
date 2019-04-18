6107451908a199ad4af84388ba26481a95a9cb11
trongbinh15 <trongbinhnguyen15@gmail.com>
2019-04-18 18:09:23 +0700

    doing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,37 @@
-import { NgModule }                  from '@angular/core';
-import { BrowserModule }             from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 // *************************
 // Custom Application imports
 // *************************
 
-import { AppRoutingModule }          from './app-routing.module';
-import { AppStateModule }            from './state/state.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppStateModule, firebaseConfig } from './state/state.module';
 
-import { AppComponent }              from './views/app.component';
+import { AppComponent } from './views/app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { PostsFacade, postReducer } from './state/posts';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     AppStateModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    EffectsModule.forRoot([PostsFacade]),
+    StoreModule.forRoot({
+      post: postReducer
+    }),
+    StoreDevtoolsModule
   ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
   providers: []
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/state/posts/post.facade.ts
+++ src/app/state/posts/post.facade.ts
@@ -1,25 +1,23 @@
-import { Injectable }                 from '@angular/core';
-import { Store }                      from '@ngrx/store';
-import { Effect, Actions }            from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Effect, Actions } from '@ngrx/effects';
 
-import { AngularFireDatabase }        from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 
-import { Observable }                 from 'rxjs/Observable';
-import { of }                         from 'rxjs/observable/of';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 import '../../utils/rxjs.operators';
 
-import {AppState} from '../state';
-import {Post} from './post.model';
-import {PostsQuery} from './post.reducer';
+import { AppState } from '../state';
+import { Post } from './post.model';
+import { PostsQuery } from './post.reducer';
 
 import { AUTHENTICATED } from '../users/user.actions';
-import * as postActions  from './post.actions';
+import * as postActions from './post.actions';
 type Action = postActions.All;
-
 
 @Injectable()
 export class PostsFacade {
-
   // ************************************************
   // Observable Queries available for consumption by views
   // ************************************************
@@ -30,50 +28,52 @@ export class PostsFacade {
   // Effects to be registered at the Module level
   // ************************************************
   @Effect()
-  init$: Observable<Action> = this.actions$.ofType(AUTHENTICATED)
-      .map(_ => new postActions.GetPost('/posts/testPost'));
-
+  init$: Observable<Action> = this.actions$
+    .ofType(AUTHENTICATED)
+    .map(_ => new postActions.GetPost('/posts/testPost'));
 
   @Effect()
-   getPost$: Observable<Action> = this.actions$.ofType(postActions.GET_POST)
-     .map((action: postActions.GetPost) => action.payload )
-     .delay(2000) // delay to show spinner
-     .mergeMap(payload => this.db.object(payload))
-     .map(post => {
-       post.pushKey = post.$key;
-       return new postActions.GetPostSuccess(post);
-     });
+  getPost$: Observable<Action> = this.actions$
+    .ofType(postActions.GET_POST)
+    .map((action: postActions.GetPost) => action.payload)
+    .delay(2000) // delay to show spinner
+    .mergeMap(payload => this.db.object(payload))
+    .map(post => {
+      post.pushKey = post.$key;
+      return new postActions.GetPostSuccess(post);
+    });
 
+  @Effect()
+  voteUpdate: Observable<Action> = this.actions$
+    .ofType(postActions.VOTE_UPDATE)
+    .map((action: postActions.VoteUpdate) => action.payload)
+    .mergeMap(payload =>
+      of(
+        this.db.object('posts/' + payload.post.pushKey).update({
+          votes: payload.post.votes + payload.val
+        })
+      )
+    )
 
-   @Effect()
-   voteUpdate: Observable<Action> = this.actions$.ofType(postActions.VOTE_UPDATE)
-     .map((action: postActions.VoteUpdate) => action.payload )
-     .mergeMap(payload => of(this.db.object('posts/' + payload.post.pushKey)
-                          .update({
-                            votes: payload.post.votes + payload.val
-                          })))
-
-     .map(() => new postActions.VoteSuccess())
-     .catch(err => of (new postActions.VoteFail( { error: err.message } )) );
+    .map(() => new postActions.VoteSuccess())
+    .catch(err => of(new postActions.VoteFail({ error: err.message })));
 
   // ************************************************
   // Internal Code
   // ************************************************
 
   constructor(
-      private actions$: Actions,
-      private store: Store<AppState>,
-      private db: AngularFireDatabase
-  ) { }
-
+    private actions$: Actions,
+    private store: Store<AppState>,
+    private db: AngularFireDatabase
+  ) {}
 
   loadPost(name = 'testPost'): Observable<Post> {
     this.store.dispatch(new postActions.GetPost(`/posts/${name}`));
     return this.post$;
   }
 
-  vote(post: Post, val: number):void {
+  vote(post: Post, val: number): void {
     this.store.dispatch(new postActions.VoteUpdate({ post, val }));
   }
-
 }
